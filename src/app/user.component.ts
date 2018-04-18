b0157825fb1d4f4389ccf48ac8c779f91fb2a67e
Rahul Kumar <rahulkumar@rediffmail.com>
2018-04-18 15:47:43 +0530

    Dynamic template added

--- src/app/user.component.ts
+++ src/app/user.component.ts
@@ -1,5 +1,17 @@
 import {Component} from '@angular/core';
 
-@co
+@Component({
+  selector: 'app-user',
+  template: `
+    <input type="text"(input)="onUserInput($event)">
+    <p>Hello {{name}}</p>
+    <p>I'am Rahul</p>
+  `
+ })
 
-export class UserComponent {}
+export class UserComponent {
+  name = 'Rahul';
+  onUserInput(event) {
+    this.name = event.target.value;
+  }
+}
